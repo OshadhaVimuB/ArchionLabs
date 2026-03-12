69b2c3bb51ff33fa0f706d4f88624a3864c51728
Sachiro <sachiro.20232147@iit.ac.lk>
2026-03-12 05:54:18 +0530

    Add live 3D preview for uploaded model using ModelViewer

--- archion-community/src/app/Components/ModelViewer.jsx
+++ archion-community/src/app/Components/ModelViewer.jsx
@@ -1,42 +1,37 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, useGLTF, Environment } from "@react-three/drei";
-import { Suspense, useEffect } from "react";
-
+import { OrbitControls, useGLTF } from "@react-three/drei";
+import { Suspense } from "react";
+import { Orbit } from "next/font/google";
 
 function Model({ url }) {
-
   const { scene } = useGLTF(url);
-
-  useEffect(() => {
-    return () => {
-      useGLTF.clear(url);
-    };
-  }, [url]);
-
-  return <primitive object={scene} scale={1} />;
+  return <primitive object={scene} scale={1.2} />;
 }
 
 export default function ModelViewer({ modelUrl }) {
 
-  if (!modelUrl) return null;
+  if (!modelUrl) {
+    return (
+      <div className="flex items-center justify-center h-full text-zinc-400">
+        No model
+      </div>
+    );
+  }
 
   return (
     <Canvas
-      camera={{ position: [4, 4, 6], fov: 60 }}
-      style={{ height: "100%", width: "100%" }}
+      camera={{ position: [2, 2, 2], fov: 50 }}
+      style={{ width: "100%", height: "100%" }}
     >
-      <ambientLight intensity={1} />
-      <directionalLight position={[5,5,5]} />
+      <ambientLight intensity={0.8} />
+      <directionalLight position={[3, 3, 3]} />
 
       <Suspense fallback={null}>
         <Model url={modelUrl} />
       </Suspense>
-
-      <OrbitControls />
-
-      <Environment preset="city" />
+      <OrbitControls enableZoom={true} enablePan={true} enableRotate={true} autoRotate autoRotateSpeed={20}/>
     </Canvas>
   );
 }
